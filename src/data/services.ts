26e443a530d8b572e21cced93287fdcc2299fd52
Romelus Jean-Marie <jean-marie@renolib.fr>
2023-11-15 23:43:22 +0100

    add services

---
+++ src/data/services.ts
@@ -0,0 +1,32 @@
+export default [
+  {
+    name: "Intégration d'intelligence artificielle",
+    description:
+      "Je suis spécialisé dans l'intégration d'intelligence artificielle pour optimiser vos processus métier.",
+  },
+  {
+    name: "Développement d'API",
+    description:
+      "J'excelle dans le développement d'API robustes et évolutives pour connecter vos applications et systèmes, favorisant ainsi l'interopérabilité, la sécurité et l'échange de données fluides.",
+  },
+  {
+    name: "Développement de smart contracts",
+    description:
+      "Je vous accompagnons dans la création et l'implémentation de smart contracts fiables et sécurisés, exploitant la blockchain pour automatiser et garantir l'exécution de vos accords contractuels.",
+  },
+  {
+    name: "Responsive design",
+    description:
+      "Mon approche du responsive design garantit une expérience utilisateur optimale sur tous les appareils. Je façonne des interfaces intuitives et adaptatives pour maximiser l'engagement de vos utilisateurs.",
+  },
+  {
+    name: "Développement logiciel SaaS",
+    description:
+      "Mon expertise dans le développement de logiciel SaaS repose sur la création de solutions logicielles fiables. Je conçois des plateformes évolutives, flexibles et sécurisées, offrant une expérience utilisateur intuitive et une gestion simplifiée, permettant ainsi à votre entreprise de prospérer dans un environnement numérique moderne.",
+  },
+  {
+    name: "Cryptographie",
+    description:
+      "Ma maîtrise de la cryptographie assure la confidentialité et l'intégrité des données. J'utilise des protocoles avancés pour sécuriser vos informations les plus sensibles.",
+  },
+];
